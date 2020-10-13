818f3e2a1ac366a5a9ceed54c1c54f7be6f3e72b
navjordj <navjordj@gmail.com>
2020-10-13 10:35:24 +0200

    remove example in autoencoder.py

--- autoencoder.py
+++ autoencoder.py
@@ -92,38 +92,3 @@ class DenseAutoencoder():
         autoencoder.summary()
 
         return autoencoder
-
-
-    
-if __name__ == "__main__":
-    (x_train, _), (x_test, _) = mnist.load_data()
-    x_train = x_train.astype('float32') / 255.
-    x_test = x_test.astype('float32') / 255.
-    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-    print(x_train.shape)
-    print(x_test.shape)
-
-    au = DenseAutoencoder(input_shape=(784,), layer_sizes=[128, 64, 32], latent_dim = 16)
-    au.fit(x_train, epochs=100, batch_size=256)
-
-    decoded = au.predict(x_test)
-
-    n = 10  # How many digits we will display
-    plt.figure(figsize=(20, 4))
-    for i in range(n):
-        # Display original
-        ax = plt.subplot(2, n, i + 1)
-        plt.imshow(x_test[i].reshape(28, 28))
-        plt.gray()
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-
-        # Display reconstruction
-        ax = plt.subplot(2, n, i + 1 + n)
-        plt.imshow(decoded_imgs[i].reshape(28, 28))
-        plt.gray()
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-    plt.show()
-
