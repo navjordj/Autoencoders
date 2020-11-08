f0b23bc63984f63d526cc70ee0fe44638f1a1f54
Jørgen Navjord <navjordj@gmail.com>
2020-11-08 22:05:58 +0100

    implement predict in conv autoencoder

--- conv_autoencoder/conv_autoencoder.py
+++ conv_autoencoder/conv_autoencoder.py
@@ -3,6 +3,7 @@ from tensorflow.keras import Model
 from tensorflow.keras.layers import Dense, Input, Conv2D, MaxPooling2D, UpSampling2D
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.regularizers import l1
+from tensorflow.keras.layers.experimental.preprocessing import Resizing
 
 import numpy as np
 
@@ -12,13 +13,15 @@ import copy
 
 class ConvAutoencoder():
 
-    def __init__(self, input_shape, filter_sizes, kernel_size, strides=1, pool_shape=(2, 2), skip_last_padding=True, activation="relu"):
+    def __init__(self, input_shape, filter_sizes, kernel_size, strides=1, pool_shape=(2, 2), output_channels=1, skip_last_padding=True, resize_layer=None, activation="relu"):
         self.input_shape = input_shape
         self.activation = activation
         self.filter_sizes = filter_sizes
         self.kernel_size = kernel_size
         self.strides = strides
         self.pool_shape = pool_shape
+        self.output_channels = output_channels
+        self.resize_layer = resize_layer
         self.skip_last_padding = skip_last_padding
 
         self.encoder = None
@@ -29,8 +32,8 @@ class ConvAutoencoder():
         self.autoencoder.fit(
             X, X, epochs=epochs, batch_size=batch_size, shuffle=True, callbacks=callback)
 
-    def predict(self):
-        pass
+    def predict(self, X):
+        return self.autoencoder.predict(X)
 
     def encode(self):
         raise NotImplementedError()
@@ -58,11 +61,18 @@ class ConvAutoencoder():
                          self.strides, activation=self.activation)(decoded)
         decoded = UpSampling2D(self.pool_shape)(decoded)
 
-        decoded = Conv2D(1, self.kernel_size, self.strides,
+        decoded = Conv2D(self.output_channels, self.kernel_size, self.strides,
                          padding="same", activation=self.activation)(decoded)
+
+        if self.resize_layer is not None:
+            decoded = Resizing(128, 128)(decoded)
+
 
         model = Model(input, decoded)
         model.summary()
+        model.compile(optimizer="adam", loss="binary_crossentropy")
+
+        return model
 
     def _conv_output_shape(self):
         pass
